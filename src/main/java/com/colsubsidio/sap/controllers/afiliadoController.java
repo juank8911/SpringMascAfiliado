131e2287971c4b428f244f4c89e75178e602f52c
jromo <jromo@softmanagment.com.co>
2022-10-10 12:55:01 -0500

    afiliados

--- src/main/java/com/colsubsidio/sap/controllers/afiliadoController.java
+++ src/main/java/com/colsubsidio/sap/controllers/afiliadoController.java
@@ -11,8 +11,11 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.ClientResponse;
+import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import reactor.core.publisher.Mono;
@@ -22,11 +25,14 @@ import com.colsubsidio.sap.apigee.ApigeeTokenResDTO;
 import com.colsubsidio.sap.apigee.ApigeeValidadorReq;
 import com.colsubsidio.sap.service.TokenService;
 
+import lombok.extern.slf4j.Slf4j;
+
 
 
 
 @RestController
 @CrossOrigin(origins = "http://localhost:4200")
+@Slf4j
 public class afiliadoController {
 	
 //	@Value("${apigee.token.url}")
@@ -36,6 +42,8 @@ public class afiliadoController {
 		private String auth = "/oauth/client_credential/accesstoken";
 		private String clienteId= "5A0amJxdlqDKntk1HbMAW7IxNrOW2026";
 		private String clienteSecreto= "A3Wtl1gzqXUcM5rQ";
+		//newCode falta url app server
+		final String urlServer="https://colsubsidio-test.apigee.net";
 	
 		
 	@GetMapping("/afiliados")
@@ -79,7 +87,8 @@ public class afiliadoController {
 	
 	
 	public Mono<String> obtainSecuredResource() {
-	    Mono<String> resource = client.post()
+		
+	    Mono<String> resource = client.post()	    		
 	      .uri("localhost:8085/oauth/token")
 	      .header("Authorization", "Basic " + {TOKEN_HERE})
 	      .body(BodyInserters.fromFormData("grant_type", "client_credentials"))
@@ -101,5 +110,31 @@ public class afiliadoController {
 	
 	
 	
-
+	}
+	
+	
+		
+	    //10/10/2022
+		@GetMapping("/afiliado/validador/{tipoId}")
+		public Mono<ResponseEntity<Mono<String>>> testGet(@PathVariable("tipoId") String tipoId) {
+			final long dateStarted = System.currentTimeMillis();
+			//?/server
+			WebClient webClient = WebClient.create(urlApi+"/afiliacion/validador");
+			Mono<ClientResponse> respuesta = webClient.get().uri("?queryParam={name}", tipoId).exchange();
+			Mono<ClientResponse> respuesta1 = webClient.get().uri("?queryParam={name}","SPEED".equals(tipoId)?"SPEED":"STOP").exchange();
+			
+			Mono<ResponseEntity<Mono<String>>> f1 = Mono.zip(respuesta, respuesta1)
+			.map(t -> {
+				if (!t.getT1().statusCode().is2xxSuccessful()) {
+					return ResponseEntity.status(t.getT1().statusCode()).body(t.getT1().bodyToMono(String.class));
+				}
+				if (!t.getT2().statusCode().is2xxSuccessful()) {
+					return ResponseEntity.status(t.getT2().statusCode()).body(t.getT2().bodyToMono(String.class));
+				}
+				return ResponseEntity.ok().body(Mono.just(
+						"All OK. Seconds elapsed: " + (((double) (System.currentTimeMillis() - dateStarted) / 1000))));
+			});
+			return f1;
+		
+	}
 }
