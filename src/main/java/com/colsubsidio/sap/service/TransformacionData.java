dcb8c001f1e255b5de6ca00c70f0b8a301cb62c4
juank8911 <ingjuanguzmane@gmail.com>
2022-10-19 21:49:02 -0500

    conversion a jsonObject

--- src/main/java/com/colsubsidio/sap/service/TransformacionData.java
+++ src/main/java/com/colsubsidio/sap/service/TransformacionData.java
@@ -5,12 +5,16 @@ import java.io.IOException;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.Iterator;
+import java.util.List;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Service;
 
 import com.colsubsidio.sap.interfaz.ITransDatos;
@@ -18,8 +22,22 @@ import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 
 
+
+
+@ConfigurationProperties(prefix = "appli")
 @Service
 public class TransformacionData implements ITransDatos {
+
+	
+@Value("${my.property.s:}")
+private String innerS;
+
+@Value("${estado.afiliado.estado:}")
+private JSONObject estado;
+
+
+
+
 
 	@Override
 	public JSONObject transData(JSONObject afil)
@@ -60,6 +78,7 @@ public class TransformacionData implements ITransDatos {
 						 				{
 						 				JSONArray pacsRt = aux.getJSONArray(key2);
 						 				pacsRt = TransDatosPacs(pacsRt);
+						 				resp.put(key2,pacsRt);
 						 				}
 						 			
 						 			}
@@ -75,7 +94,9 @@ public class TransformacionData implements ITransDatos {
 			 else 
 			 {
 //				 System.out.println("no es array");
-				 resp.append(key, afil.get(key));
+				 resp.put(key, afil.get(key));
+				 
+				 
 			 }
 //			 System.out.println(resp.toString());
 	
@@ -84,26 +105,35 @@ public class TransformacionData implements ITransDatos {
 		return resp;
 	}
 
-	private JSONArray TransDatosPacs(JSONArray pacs) throws JSONException, ParseException {
+	private JSONArray TransDatosPacs(JSONArray pacs) {
 		// TODO Auto-generated method stub
 		JSONArray respPacs = new JSONArray();
 			for(int i=0;i<pacs.length();i++)
 			{
-				JsonObject JsoBenf = new JsonObject();
-//				System.out.println(pacs.length());
+//				JsonObject  = new JsonObject();
+				JSONObject JsoBenf = new JSONObject();
+//				JsoBenf.addProperty("token", currentTokenNo.replaceAll("\"","").replaceAll("/","")); 
 				JSONObject auxPac = pacs.getJSONObject(i);
+				JsoBenf.put("documento", "1234");
 				System.out.println(auxPac.toString());		
-				JsoBenf.addProperty("numeroDocumento", auxPac.get("numeroDocumento").toString());
-				JsoBenf.addProperty("bNombre", auxPac.get("primerNombre").toString() +" "+auxPac.get("segundoNombre").toString()+" "+auxPac.get("apellido").toString()+" "+auxPac.get("segundoApellido").toString());
-				JSONObject relacion = auxPac.getJSONObject("relacion");
-				JsoBenf.addProperty("relacion", relacion.get("descripcion").toString());
-				JsoBenf.addProperty("genero", auxPac.get("genero").toString().equals("1")?"Femenino":"Masculino");
-				System.out.println(auxPac.get("fechaNacimiento"));
+//				JsoBenf.addProperty("numeroDocumento", (String) auxPac.get("numeroDocumento"));
+//				JsoBenf.addProperty("prueba", auxPac.get("numeroDocumento").toString());
+//				JsoBenf.addProperty("bNombre", auxPac.get("primerNombre") +" "+auxPac.get("segundoNombre")+" "+auxPac.get("apellido")+" "+auxPac.get("segundoApellido"));
+//				JSONObject relacion = auxPac.getJSONObject("relacion");
+//				JsoBenf.addProperty("relacion", (String) relacion.get("descripcion"));
+//				JsoBenf.addProperty("genero", auxPac.get("genero").toString().equals("1")?"Femenino":"Masculino");
+//				System.out.println(auxPac.get("fechaNacimiento"));
+//				convertirFecha(auxPac.get("fechaNacimiento").toString());
+//				String estd = convertirEstado(auxPac.get("estado").toString(), estado);
+//				JsoBenf.addProperty("estado", estd);
+//				JsoBenf.addProperty("discapacidad", auxPac.getString("discapacidad").equals("null")?"":auxPac.getString("discapacidad"));
 				
-				convertirFecha(auxPac.get("fechaNacimiento").toString());
-				System.out.println(JsoBenf.toString());
+				
+				System.out.println(JsoBenf);
+				respPacs.put(JsoBenf);
 			}
-		return pacs;
+			System.out.println(respPacs);	
+		return respPacs;
 	}
 	
 	
@@ -114,6 +144,12 @@ public class TransformacionData implements ITransDatos {
 		Date fecha1 = formato.parse(fecha);
 		System.out.println("dentro de convertir fecha");
 		System.out.println(fecha1);
+	}
+	
+	public String convertirEstado(String estadoAp, JSONObject estYml)
+	{
+		String estad = estYml.get(estadoAp).toString();
+		return estad;
 	}
 
 	@Override
