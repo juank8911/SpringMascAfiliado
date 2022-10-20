140e6bd1a9150e98555c681ac208ab4147f30e74
juank8911 <ingjuanguzmane@gmail.com>
2022-10-20 09:41:56 -0500

    ayda fecha

--- src/main/java/com/colsubsidio/sap/service/TransformacionData.java
+++ src/main/java/com/colsubsidio/sap/service/TransformacionData.java
@@ -32,8 +32,16 @@ public class TransformacionData implements ITransDatos {
 @Value("${my.property.s:}")
 private String innerS;
 
-@Value("${estado.afiliado.estado:}")
+@Value("${estado.estado:}")
 private JSONObject estado;
+
+@Value("${estado.tarjeta:}")
+private JSONObject eTarjeta;
+
+
+@Value("${tipo.tra:}")
+private JSONObject trabajo;
+
 
 
 
@@ -43,6 +51,8 @@ private JSONObject estado;
 	public JSONObject transData(JSONObject afil)
 	{
 		JSONObject resp = new JSONObject();
+		JSONObject auxData = new JSONObject();
+		JSONArray fnData = new JSONArray();
 		JSONArray data = afil.getJSONArray("data");
 //		System.out.println(data);
 		Iterator<String> keys = afil.keys();
@@ -61,7 +71,7 @@ private JSONObject estado;
 				 for(int i=0;i<auxdt.length();i++)
 				 {
 					 JSONObject aux = auxdt.getJSONObject(i);
-					 System.out.println(aux instanceof JSONObject);
+//					 System.out.println(aux instanceof JSONObject);
 					 
 					 if(aux instanceof JSONObject)
 					 {
@@ -70,23 +80,33 @@ private JSONObject estado;
 						 			{
 						 				String key2 = keys2.next();
 //						 				System.out.println(key2);
-						 				System.out.println(aux.get(key2).getClass());
-						 				//tratar datos de pacs
+//						 				System.out.println(aux.get(key2));
+//						 				System.out.println(aux.get(key2).getClass());
 						 				//tratar datos de afiliado
 						 				//tratar Datos de empleadores
+						 				if("afiliado".equals(key2) && aux.get(key2) instanceof JSONObject)
+						 				{
+//						 				System.out.println("dentro del afiliados");
+						 				JSONObject afilRpt = aux.getJSONObject(key2);
+//						 				System.out.println(afilRpt);
+//						 				JSONArray afilRt = aux.getJSONArray(key2);
+						 				afilRpt = TransDatosAfili(afilRpt);
+						 				auxData.put(key2,afilRpt);
+						 				}
 						 				if("pacs".equals(key2) && aux.get(key2) instanceof JSONArray)
 						 				{
 						 				JSONArray pacsRt = aux.getJSONArray(key2);
 						 				pacsRt = TransDatosPacs(pacsRt);
-						 				resp.put(key2,pacsRt);
+						 				auxData.put(key2,pacsRt);
 						 				}
 						 			
 						 			}
-						 
+						 			fnData.put(auxData);
+						 			resp.put("data",fnData);
 					 }
 					 else
 					 {
-						 System.out.println("no esw objeto");
+//						 System.out.println("no esw objeto");
 					 }
 					 
 				 }
@@ -105,6 +125,49 @@ private JSONObject estado;
 		return resp;
 	}
 
+	private JSONObject TransDatosAfili(JSONObject afilRpt) {
+		// TODO Auto-generated method stub
+		JSONObject resAfil = new JSONObject();
+		JSONObject jsAuxAf = afilRpt.getJSONObject("afiliacion"); //auxuliar Afiliacion
+		JSONObject auxMulSer = afilRpt.getJSONObject("tarjetaMultiservicios"); //auxiliar tarjeta multi
+		JSONObject auxMtPa = auxMulSer.getJSONObject("metodoPago");
+		resAfil.put("nombre", afilRpt.get("primerNombre")+" "+afilRpt.get("segundoNombre")+" "+afilRpt.get("primerApellido")+" "+afilRpt.get("segundoApellido"));
+		resAfil.put("fechaNacimiento", afilRpt.get("fechaNacimiento"));
+//		System.out.println(jsAuxAf);
+		resAfil.put("fechaAfiliacion", jsAuxAf.get("fechaAfiliacion"));
+		String estd = convertirEstado(afilRpt.get("estado").toString(), estado);
+		resAfil.put("estado",estd);
+		resAfil.put("fechaAfiliacion", jsAuxAf.get("fechaRetiro"));
+		resAfil.put("categoria", jsAuxAf.get("categoria"));
+		String tipoTr = convertirEstado(jsAuxAf.get("tipoTrabajador").toString(), trabajo);
+		resAfil.put("tipoTrabajador", tipoTr);
+		resAfil.put("tarjetaMultiservicios",auxMulSer.get("numeroTarjeta"));
+		resAfil.put("gp",auxMulSer.get("gp"));
+		String eEntre = convertirEstado(auxMulSer.get("estadoEntrega").toString(), eTarjeta);
+		resAfil.put("estadoEntrega",eEntre);
+		resAfil.put("fechaEntrega",auxMulSer.get("fechaEntrega"));
+		resAfil.put("subsidio", jsAuxAf.get("subsidio"));
+		resAfil.put("ultimoMesPagado",auxMulSer.get("ultimoMesPagado"));
+		resAfil.put("metodoPago", auxMtPa.get("nombre"));
+		resAfil.put("motivoBloqueo", auxMulSer.get("motivoBloqueo"));
+		resAfil.put("porcentajeApor", jsAuxAf.get("porcentajeAporte")+"%");
+		resAfil.put("fechaRetiro", jsAuxAf.get("fechaRetiro"));
+		resAfil.put("numeroDocumento", afilRpt.get("numeroDocumento"));
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+//		System.out.println(resAfil);
+		return resAfil;
+	}
+
 	private JSONArray TransDatosPacs(JSONArray pacs) {
 		// TODO Auto-generated method stub
 		JSONArray respPacs = new JSONArray();
@@ -114,25 +177,33 @@ private JSONObject estado;
 				JSONObject JsoBenf = new JSONObject();
 //				JsoBenf.addProperty("token", currentTokenNo.replaceAll("\"","").replaceAll("/","")); 
 				JSONObject auxPac = pacs.getJSONObject(i);
-				JsoBenf.put("documento", "1234");
-				System.out.println(auxPac.toString());		
-//				JsoBenf.addProperty("numeroDocumento", (String) auxPac.get("numeroDocumento"));
-//				JsoBenf.addProperty("prueba", auxPac.get("numeroDocumento").toString());
-//				JsoBenf.addProperty("bNombre", auxPac.get("primerNombre") +" "+auxPac.get("segundoNombre")+" "+auxPac.get("apellido")+" "+auxPac.get("segundoApellido"));
-//				JSONObject relacion = auxPac.getJSONObject("relacion");
-//				JsoBenf.addProperty("relacion", (String) relacion.get("descripcion"));
-//				JsoBenf.addProperty("genero", auxPac.get("genero").toString().equals("1")?"Femenino":"Masculino");
-//				System.out.println(auxPac.get("fechaNacimiento"));
-//				convertirFecha(auxPac.get("fechaNacimiento").toString());
-//				String estd = convertirEstado(auxPac.get("estado").toString(), estado);
-//				JsoBenf.addProperty("estado", estd);
-//				JsoBenf.addProperty("discapacidad", auxPac.getString("discapacidad").equals("null")?"":auxPac.getString("discapacidad"));
+//				JsoBenf.put("documento", "1234");
+//				System.out.println(auxPac.toString());		
+				JsoBenf.put("numeroDocumento", (String) auxPac.get("numeroDocumento"));
+				JsoBenf.put("bNombre", auxPac.get("primerNombre") +" "+auxPac.get("segundoNombre")+" "+auxPac.get("apellido")+" "+auxPac.get("segundoApellido"));
+				JSONObject relacion = auxPac.getJSONObject("relacion");
+				JsoBenf.put("relacion", (String) relacion.get("descripcion"));
+				JsoBenf.put("genero", auxPac.get("genero").toString().equals("1")?"Femenino":"Masculino");
+				try {
+					convertirFecha(auxPac.get("fechaNacimiento").toString());
+				} catch (JSONException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (ParseException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				JsoBenf.put("fechaNacimineto", auxPac.get("fechaNacimiento"));
+				JsoBenf.put("fechaInicioVigencia", auxPac.get("fechaInicioVigencia"));
+				String estd = convertirEstado(auxPac.get("estado").toString(), estado);
+				JsoBenf.put("estado", estd);
+				JsoBenf.put("discapacidad", auxPac.getString("discapacidad").equals("null")?"":auxPac.getString("discapacidad"));
 				
 				
-				System.out.println(JsoBenf);
+//				System.out.println(JsoBenf);
 				respPacs.put(JsoBenf);
 			}
-			System.out.println(respPacs);	
+//			System.out.println(respPacs);	
 		return respPacs;
 	}
 	
@@ -140,10 +211,12 @@ private JSONObject estado;
 	//este metodo necesito que lo realices 
 	public void convertirFecha(String fecha) throws  ParseException
 	{
-		SimpleDateFormat formato = new SimpleDateFormat("DD/MM/YYYY"); 
-		Date fecha1 = formato.parse(fecha);
+		System.out.println(fecha);
+		String fh = "1953-09-25";
+		SimpleDateFormat formato = new SimpleDateFormat("DD-MM-YYYY"); 
+		Date fecha1 = formato.parse(fh);
 		System.out.println("dentro de convertir fecha");
-		System.out.println(fecha1);
+		System.out.println(fecha1.toString());
 	}
 	
 	public String convertirEstado(String estadoAp, JSONObject estYml)
